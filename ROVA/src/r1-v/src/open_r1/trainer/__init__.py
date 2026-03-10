aa88f3070513fe4e0e95920721eaaee4447011b3
Yangfan He <116295741+codepassionor@users.noreply.github.com>
2026-03-10 22:43:51 +0800

    Delete ROVA directory

--- ROVA/src/r1-v/src/open_r1/trainer/__init__.py
+++
@@ -1,7 +0,0 @@
-from .grpo_trainer import Qwen2VLGRPOTrainer
-from .vllm_grpo_trainer_modified import Qwen2VLGRPOVLLMTrainerModified
-
-__all__ = [
-    "Qwen2VLGRPOTrainer", 
-    "Qwen2VLGRPOVLLMTrainerModified"
-]
